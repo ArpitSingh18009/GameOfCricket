120cb63d5bc2d914bf85fe7e600e55c2cc0a86c8
Aditya <akgupta@tekion.com>
2023-01-24 01:20:09 +0530

    first commit

--- src/Batsman.java
+++ src/Batsman.java
@@ -2,5 +2,6 @@ public class Batsman extends Player
 {
     public Batsman(String name, int age, String teamName) {
         super(name, age, teamName);
+        System.out.println("batsman.java");
     }
 }
